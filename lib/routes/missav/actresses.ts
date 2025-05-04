ecc9309f3ea3c5b85ef4f44578f5694b0286d524
UnlockHomes <edward.chiang1010@gmail.com>
2025-05-04 10:59:46 +0100

    Create actresses.ts

---
+++ lib/routes/missav/actresses.ts
@@ -0,0 +1,29 @@
+import { Route } from '@/types';
+import ofetch from '@/utils/ofetch';
+import cheerio from 'cheerio';
+import config from './namespace';
+
+const route: Route = {
+  endpoint: '/missav/actresses/:query',
+  async handler(ctx) {
+    const q = encodeURIComponent(ctx.params.query);
+    const url = `${config.url}/dm39/cn/actresses/${q}`;
+    const html = await ofetch(url, { headers: { 'User-Agent': config.ua } });
+    const $ = cheerio.load(html);
+    const items = $('.video-card').map((_, el) => {
+      const el$ = $(el);
+      return {
+        title: el$.find('.video-title').text().trim(),
+        link: config.url + el$.find('a').attr('href'),
+        description: `<img src="${el$.find('img').attr('src')}"><br>${el$.find('.video-meta').text().trim()}`,
+      };
+    }).get();
+    ctx.state.data = {
+      title: `MissAV Actresses: ${ctx.params.query}`,
+      link: url,
+      item: items,
+    };
+  },
+};
+
+export default route;
